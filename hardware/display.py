dea1fac9c71f537a8d01be26139e325e6645a0ad
Yu-Fan Tung <yufan.tung@gmail.com>
2025-07-12 00:16:00 +0800

    Switch dio and clk

--- hardware/display.py
+++ hardware/display.py
@@ -10,8 +10,8 @@ cfg = ConfigManager()
 
 class DisplayManager:
     def __init__(self):
-        clk_pin = cfg.get_int("display_clk_pin", default=22)
-        dio_pin = cfg.get_int("display_dio_pin", default=27)
+        clk_pin = cfg.get_int("display_clk_pin", default=27)
+        dio_pin = cfg.get_int("display_dio_pin", default=22)
 
         # 明確指定 gpiochip，根據你環境是 /dev/gpiochip4
         self.display = tm1637.TM1637(clk=clk_pin, dio=dio_pin, chip_path="/dev/gpiochip4")
